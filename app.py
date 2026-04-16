ba865891802ab2f26c9afc1659f5d1c2354b0a75
tg-bok <rank8880@gmail.com>
2026-04-16 06:21:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-gunicorn "bot_app_step51:create_production_app()" --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 120
+gunicorn "app:create_production_app()" --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 120
